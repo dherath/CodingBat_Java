54da07533f96f72966e7246f62f42e580b74a470
Dinal Herath <dinal.bing@gmail.com>
2017-08-17 09:27:20 -0400

    Array-3 problem 4 : attemp 1

---
+++ Array-3/canBalance.java
@@ -0,0 +1,18 @@
+public boolean canBalance(int[] nums) {
+  for(int i=0;i<nums.length;i++){
+    int sum1 = findSum(Arrays.copyOfRange(nums,0,i));
+    int sum2 = findSum(Arrays.copyOfRange(nums,i+1,nums.length-1));
+    if (sum1==sum2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+public int findSum(int[] array) {
+  int sum = 0;
+  for(int temp:array){
+    sum += temp;
+  }
+  return sum;
+}
