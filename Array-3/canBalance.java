36d79aeeb8ee56a499085046a5ff814cf93620d2
Dinal Herath <dinal.bing@gmail.com>
2017-08-17 11:24:47 -0400

    Array-3 problem 4: complete

--- Array-3/canBalance.java
+++ Array-3/canBalance.java
@@ -2,26 +2,26 @@ import java.util.Arrays;
 
 public class canBalance{
 
-  public boolean canBalance(int[] nums) {
-    for(int i=0;i<nums.length-1;i++){
-      int sum1 = findSum(Arrays.copyOfRange(nums,0,i));
-      int sum2 = findSum(Arrays.copyOfRange(nums,i+1,nums.length-1));
-      if (sum1==sum2) {
-        return true;
-      }
+    public boolean canBalance(int[] nums) {
+	for(int i=0;i<nums.length-1;i++){
+	    int sum1 = findSum(Arrays.copyOfRange(nums,0,i+1));
+	    System.out.println("sum1 "+sum1+" iteration "+i);
+	    int sum2 = findSum(Arrays.copyOfRange(nums,i+1,nums.length));
+	    System.out.println("sum2 "+sum2+" iteration "+i);
+	    if (sum1==sum2) {
+		return true;
+	    }
+	}
+	return false;
     }
-    return false;
-  }
 
-  public int findSum(int[] array) {
-    int sum = 0;
-    for(int temp:array){
-      sum += temp;
+    public int findSum(int[] array) {
+	int sum = 0;
+	for(int temp:array){
+	    sum += temp;
+	}
+	return sum;
     }
-    return sum;
-  }
 
 
 }
-
-
