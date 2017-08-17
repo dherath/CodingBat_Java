aab0117c5959a7f4fdc21e733877a3ed047091ea
Dinal Herath <dinal.bing@gmail.com>
2017-08-17 11:50:49 -0400

    Array-3 problem 2 : attemp 1

---
+++ Array-3/fix34Order.java
@@ -0,0 +1,23 @@
+public class fix34Order{
+    public int[] fix34(int[] nums) {
+	int indexOf3 = findLocation(nums,3);
+	int indexOf4 = findLocation(nums,4);
+	for (int i=indexOf4; i>indexOf3;i--){
+	    int temp = nums[i-1];
+	    nums[i-1] = nums[i];
+	    nums[i]=temp;
+	}
+	return nums;
+    }
+
+    public int findLocation(int[] array,int number) {
+	int temp = array[0];
+	int index = 0;
+	while(temp != number && index<=array.length) {
+	    index++;
+	    temp = array[index];
+	}
+	return index;
+    }
+    
+}
---
+++ Array-3/testFix34Order.java
@@ -0,0 +1,21 @@
+public class testFix34Order extends fix34Order{
+    public static void main(String args[]) {
+	int[] temp1 = {1,2,3,5,6,4,3};
+	int[] temp2 = {5,6,5,3,6,4,1};
+
+	fix34Order func = new fix34Order();
+
+	System.out.print("old array [");
+	for(int val:temp1){
+	    System.out.print(val+" ");
+	}
+	System.out.println("]");
+
+	System.out.print("new array [");
+	//int[] out1 = func.fix34Order(temp1);
+	for(int val:func.fix34(temp1)){
+	    System.out.print(val+" ");
+	}
+	System.out.println("]");
+    }
+}
