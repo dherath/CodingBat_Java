69f9e1372e8c7083880e8539a6639434dc199b67
Dinal Herath <dinal.bing@gmail.com>
2017-08-17 09:30:31 -0400

    Array-3 problem 4: attampt 2 (>75% working)

--- Array-3/canBalance.java
+++ Array-3/canBalance.java
@@ -1,5 +1,5 @@
 public boolean canBalance(int[] nums) {
-  for(int i=0;i<nums.length;i++){
+  for(int i=0;i<nums.length-1;i++){
     int sum1 = findSum(Arrays.copyOfRange(nums,0,i));
     int sum2 = findSum(Arrays.copyOfRange(nums,i+1,nums.length-1));
     if (sum1==sum2) {
